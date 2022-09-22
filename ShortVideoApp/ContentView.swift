b6e77f49ea21cbbd375ba7e738b62037a8f682f2
GermanoCostaUX <cgermano.fcosta.nnt@gmail.com>
2022-09-22 15:04:11 -0300

    Initial Commit

---
+++ ShortVideoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShortVideoApp
+//
+//  Created by user on 22/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShortVideoApp/ShortVideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortVideoAppApp.swift
+//  ShortVideoApp
+//
+//  Created by user on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ShortVideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
